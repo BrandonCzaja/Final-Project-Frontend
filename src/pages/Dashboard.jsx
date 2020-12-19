121c591d71acdb3a0dcc774534b4b86286d69035
Brandon Czaja <Brandon.Czaja@gmail.com>
2020-12-19 16:31:25 -0600

    Added some concept notes

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Dashboard = (props) => {
+  // This will be the user's home page. From here they should be able to look up plants.
+  // Stretch goal will be for them to save plants and add notes to them
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 const Landing = (props) => {
+  // User should see message that says "Welcome to AppName, please sign up or log in"
+  // Stretch Goal: "See my other app, Park Life"
   return <h1>Landing</h1>;
 };
 
