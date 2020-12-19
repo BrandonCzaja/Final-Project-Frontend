e0afd137b57e9c226973f13852fae94335694721
Brandon Czaja <Brandon.Czaja@gmail.com>
2020-12-19 15:43:45 -0600

    Index.jsx
    Imported BrowserRouter as Router and wrapped App component in Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export const App = (props) => {
+  // url
+  // State and setState for plants
+  // Function that will grab the plants from the backend with fetch
+  // React use effect to get the plants
+  // Loaded function that can be used for turnary operator to display data when available otherwise loading... Need to map through the plants... Can give each of them a note button that takes the user to a new page and from there They can delete notes or add notes
+  // Create a form that has createChange / handleChange / submit / handleSubmit to track state in the form
+  // Return statement
+
   return <h1>Hello World</h1>;
 };
