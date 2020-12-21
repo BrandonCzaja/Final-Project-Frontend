05c6e57800cc43b7f56bbf90acf358123e31884b
Brandon Czaja <Brandon.Czaja@gmail.com>
2020-12-21 16:25:59 -0600

    Logout and Plants nav buttons only show up when user is logged in

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAppState } from "../AppState.jsx";
 
 const Nav = (props) => {
+  const { state, dispatch } = useAppState();
+
   return (
     <header>
       <h1>Plant App</h1>
@@ -9,12 +12,31 @@ const Nav = (props) => {
         <Link to="/">
           <div>Home</div>
         </Link>
-        <Link to="/auth/signup">
-          <div>Sign Up</div>
-        </Link>
-        <Link to="/auth/login">
-          <div>Log In</div>
-        </Link>
+        {state.token ? (
+          <Link to="/plants">
+            <div>Plants</div>
+          </Link>
+        ) : null}
+        {!state.token ? (
+          <>
+            <Link to="/auth/signup">
+              <div>Sign Up</div>
+            </Link>
+            <Link to="/auth/login">
+              <div>Log In</div>
+            </Link>{" "}
+          </>
+        ) : null}
+        {state.token ? (
+          <div
+            onClick={() => {
+              dispatch({ type: "logout" });
+              props.history.push("/");
+            }}
+          >
+            Logout
+          </div>
+        ) : null}
       </nav>
     </header>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = (props) => {
   // User should see message that says "Welcome to AppName, please sign up or log in"
   // Stretch Goal: "See my other app, Park Life"
-  return <h1>Plant App</h1>;
+  return <h1>Plant App Home Page</h1>;
 };
 
 export default Home;
