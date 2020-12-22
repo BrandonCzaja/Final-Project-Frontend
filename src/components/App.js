e43b0a5803a2462e285dbaad971064d49b3f8cef
Brandon Czaja <Brandon.Czaja@gmail.com>
2020-12-22 16:09:59 -0600

    Testing Deployment

--- src/components/App.js
+++ src/components/App.js
@@ -28,6 +28,7 @@ export const App = (props) => {
 
   return (
     <>
+      <h1>Hello from App.js</h1>
       <Route path="/" component={Nav} />
       <Switch>
         <Route exact path="/" component={Home} />
