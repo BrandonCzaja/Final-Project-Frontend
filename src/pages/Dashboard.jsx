03bd69fb0320a9b63c6cd7795636bd9502f45a83
Brandon Czaja <Brandon.Czaja@gmail.com>
2020-12-22 19:52:38 -0600

    Working through bugs to get plants to render on page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,49 +3,11 @@ import { Link, useParams } from "react-router-dom";
 import { useAppState } from "../AppState.jsx";
 
 const Dashboard = (props) => {
-  // Grab state and dispatch from useAppState
-  const { state, dispatch } = useAppState();
-  // Destructor token and url from state
-  const { token, url, plants } = state;
-  console.log(url);
-
-  //Function to get plants
-  const getPlants = async () => {
-    // Get request from state url and endpoint
-    const response = await fetch(url + "/plants", {
-      method: "get",
-      headers: {
-        Authorization: "bearer " + token,
-      },
-    });
-
-    const plants = response.json();
-
-    dispatch({ type: "getPlants", payload: plants });
-  };
-
-  // Run getPlants at least once
-  React.useEffect(() => {
-    getPlants();
-  }, []);
-
-  const loaded = () => (
-    <div className="dashboard">
-      <ul>
-        {plants.map((plant) => {
-          <div className="plant" key={plant.id}>
-            <h2>{plant.common_name}</h2>
-            <h2>{plant.scientific_name}</h2>
-            <h2>{plant.family}</h2>
-            <h2>{plant.genus}</h2>
-            <img src={plant.image} alt={plant.common_name} />
-          </div>;
-        })}
-      </ul>
-    </div>
+  return (
+    <>
+      <h1>Welcome to the Dashboard</h1>
+    </>
   );
-
-  return plants ? loaded() : <h1>Loading...</h1>;
 };
 
 export default Dashboard;
--- src/pages/Plants.jsx
+++ src/pages/Plants.jsx
@@ -3,31 +3,52 @@ import { Link, useParams } from "react-router-dom";
 import { useAppState } from "../AppState.jsx";
 
 const Plants = (props) => {
+  // Grab state and dispatch from useAppState
   const { state, dispatch } = useAppState();
+  // Destructor token and url from state
   const { token, url, plants } = state;
+  console.log(url);
 
+  //Function to get plants
   const getPlants = async () => {
-    const response = fetch(url + "/plants");
-    const plants = await response.json();
-    dispatch({ type: "getPlants", payload: plants });
+    // Get request from state url and endpoint
+    const response = await fetch(url + "/plants", {
+      method: "get",
+      headers: {
+        Authorization: "bearer " + token,
+      },
+    });
+
+    const fetchedPlants = await response.json();
+
+    dispatch({ type: "getPlants", payload: fetchedPlants });
   };
-  return (
-    <>
-      <h1>Hello from Plants Page</h1>
-      {/* <ul>
-        {plants.map((plant) => {
-          <div className="plant" key={plant.id}>
-            <h2>{plant.common_name}</h2>
-            <h3>{plant.scientific_name}</h3>
-            <h3>{plant.year}</h3>
-            <h3>{plant.family}</h3>
-            <h3>{plant.genus}</h3>
-            <img src={plant.image} />
-          </div>;
-        })}
-      </ul> */}
-    </>
-  );
+
+  // Run getPlants at least once
+  React.useEffect(() => {
+    getPlants();
+  }, []);
+
+  const loaded = () => {
+    console.log(state);
+    return (
+      <div className="dashboard">
+        <ul>
+          {plants.map((plant) => (
+            <div className="plant" key={plant.id}>
+              <h2>{plant.common_name}</h2>
+              <h2>{plant.scientific_name}</h2>
+              <h2>{plant.family}</h2>
+              <h2>{plant.genus}</h2>
+              <img src={plant.image} alt={plant.common_name} />
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+  return plants ? loaded() : <h1>Loading...</h1>;
 };
 
 export default Plants;
